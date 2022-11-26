6c880f488f3b32caa433b45c822d6abc45dedcca
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2022-11-26 19:19:26 +0530

    fixed formating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Container,  Main, Footer, Travel, Navbar, Banner, Stat } from "@components";
+import { Container, Main, Footer, Travel, Navbar, Banner, Stat } from "@components";
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Navbar />
-      <Main title="Uttrakhand Tourism" btnTitle="" btnbg="" height={400}/>
-      <Travel/>
-      <Banner/>
-      <Stat/>
+      <Main title="Uttrakhand Tourism" btnTitle="" btnbg="" height={400} />
+      <Travel />
+      <Banner />
+      <Stat />
       <Footer />
     </Container>
   );
